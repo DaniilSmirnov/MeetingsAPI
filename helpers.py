d92fb7c89f6c0244c1235c94916d210e85a76d8d
di.smirnov <d.smirnov2000@mail.ru>
2020-05-01 20:05:47 +0400

    optimize quality

--- helpers.py
+++ helpers.py
@@ -151,6 +151,6 @@ def compress_blob(image):
     image = image.resize((160, 300), Image.ANTIALIAS)
 
     buffered = BytesIO()
-    image.save(buffered, format="JPEG", optimize=True, quality=75)
+    image.save(buffered, format="JPEG", optimize=True, quality=90)
     image = base64.b64encode(buffered.getvalue())
     return image
