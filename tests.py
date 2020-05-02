c1ad2a2331377e7233f78df29c25d4a882da1733
di.smirnov <d.smirnov2000@mail.ru>
2020-05-02 20:21:00 +0400

    isFirst

--- tests.py
+++ tests.py
@@ -2,11 +2,11 @@ import requests
 import pytest
 
 host = "https://vargasoff.ru:8000/"
-headers = {'referer': 'https://stage-app7215168-2ea62ec052e4.pages.vk-apps.com/index.html?vk_access_token_settings=notify&vk_app_id=7217332&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=mobile_android&vk_ref=other&vk_user_id=87478742&sign=mawhkNkflRJHKqBWvPwmKxnHMIX6txGV9gA0YAiznrE'}
+headers = {
+    'referer': 'https://stage-app7215168-2ea62ec052e4.pages.vk-apps.com/index.html?vk_access_token_settings=notify&vk_app_id=7217332&vk_are_notifications_enabled=0&vk_is_app_user=1&vk_is_favorite=0&vk_language=ru&vk_platform=mobile_android&vk_ref=other&vk_user_id=87478742&sign=mawhkNkflRJHKqBWvPwmKxnHMIX6txGV9gA0YAiznrE'}
 
 
 def test_GetMeets():
-
     r = requests.get(host + 'GetMeets', headers=headers)
     assert r.status_code == 200, "Запрос не был успешным"  # Проверяем, что ответ вообще пришел
 
@@ -30,8 +30,14 @@ def test_GetMeets():
 
 
 def test_Auth():
-    r = requests.get(host + 'GetMeets') # делаем запрос без нужного хедера
-    assert r.status_code == 403, "Авторизация не сработала"  # Проверяем, что ответ вообще пришел
+    r = requests.get(host + 'GetMeets')  # делаем запрос без нужного хедера
+    assert r.status_code == 403, "Авторизация не сработала"  # Проверяем код ответа
+
+
+def isFirst():
+    r = requests.get(host + 'isFirst', headers=headers)
+    r = r.json()
+    assert r == True, "Для известного юзера пришел False"
 
 
 test_GetMeets()
